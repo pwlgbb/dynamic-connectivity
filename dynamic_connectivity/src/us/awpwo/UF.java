61474cc0e4b01cb62a4307439c723b9885eed086
pwlgbb <Student@10.2.5.3>
2016-01-09 14:20:05 +0100

    quick_union

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -3,19 +3,28 @@ package us.awpwo;
 public class UF {
 
 	protected int[] id;
-	
+
 	public UF(int N) {
 		id = new int[N];
 		for (int i = 0; i < N; ++i) {
 			id[i] = i;
 		}
 	}
-	
-	public void union(int p, int q) {
+
+	private int root(int i) {
+		while (i != id[i]) {
+			i = id[i];
+		}
+		return i;
 	}
-	
+
 	public boolean connected(int p, int q) {
-		return false;
+		return root(p) == root(q);
 	}
-	
+
+	public void union(int p, int q) {
+		int i = root(p);
+		int j = root(q);
+		id[i] = j;
+	}
 }
