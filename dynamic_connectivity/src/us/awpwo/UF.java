054fcefe2f93bcf1c0a6e794267478019f94c64d
pwlgbb <Student@10.2.5.3>
2016-01-09 14:23:20 +0100

    weighten_quick_union

--- dynamic_connectivity/src/us/awpwo/UF.java
+++ dynamic_connectivity/src/us/awpwo/UF.java
@@ -3,11 +3,14 @@ package us.awpwo;
 public class UF {
 
 	protected int[] id;
+	private int[] sz;
 
 	public UF(int N) {
 		id = new int[N];
+		sz = new int[N];
 		for (int i = 0; i < N; ++i) {
 			id[i] = i;
+			sz[i] = N;
 		}
 	}
 
@@ -25,6 +28,14 @@ public class UF {
 	public void union(int p, int q) {
 		int i = root(p);
 		int j = root(q);
-		id[i] = j;
+		if (i == j)
+			return;
+		if (sz[i] < sz[j]) {
+			id[i] = j;
+			sz[j] += sz[i];
+		} else {
+			id[j] = i;
+			sz[i] += sz[j];
+		}
 	}
 }
